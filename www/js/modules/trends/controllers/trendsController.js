5fa8b5c309f02217bc743a086911be77af7d627a
rossobrien <ross.obrien@gmail.com>
2014-11-04 21:15:02 -0800

    Add MPG chart

--- www/js/modules/trends/controllers/trendsController.js
+++ www/js/modules/trends/controllers/trendsController.js
@@ -5,5 +5,21 @@
  * @param  Tanks Tank object for interacting with API
  */
 angular.module('trendsApp').controller('TrendsController', function($scope, Tanks) {
+	$scope.FormatXAxisTick = function() {
+		return function(d) {
+			return d3.time.format('%x')(new Date(d));
+		}
+	}
+
+	Tanks.query('', function(data) {
+		var values = data.map(function(tank, i) {return [i, d3.round(tank.mpg, 2)]});
+		$scope.chartData = [
+			{
+				"key": "Miles Per Gallon",
+				"values": values
+			}
+		];
+	});
+				
 
 });
